67c6b0467846410848689f34d214b51ec82c9c98
vuduc153 <vuductran153@gmail.com>
2024-07-24 11:35:49 +1000

    Switched from websockets to RESTful API server for speech parsing request handling

---
+++ server/api.py
@@ -0,0 +1,49 @@
+from aiohttp import web
+import logging
+import time
+import json
+
+from vlmaps.utils.llm_utils import parse_object_goal_instruction_with_scene_graph
+from vlmaps.utils.prompt.template import PromptTemplate
+
+
+async def parse_speech(request):
+
+    logger.info(f"Received request {request}")
+
+    try:
+        json_msg = await request.json()
+        message = PromptTemplate.build_prompt(json_msg['past'], json_msg['current'])
+        logger.info(message)
+        result = parse_object_goal_instruction_with_scene_graph(message)
+        
+        if result is not None:
+            logger.info(result)
+            return web.json_response(json.loads(result))
+        else:
+            return web.json_response({"movements": []})
+    
+    except json.decoder.JSONDecodeError:
+        logger.error("Invalid JSON payload from client")
+        return web.Response(text="Invalid JSON payload", status=400)
+    except KeyError as e:
+        logger.error(f"KeyError: {str(e)}")
+        return web.Response(text="Missing required fields in JSON", status=400)
+    except Exception as e:
+        logger.error(f"Error processing request: {str(e)}")
+        return web.Response(text="Internal Server Error", status=500)
+
+
+async def init():
+    app = web.Application()
+    app.router.add_post('/parse', parse_speech)
+    return app
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.info("Server started")
+    
+    start = time.time()
+    web.run_app(init(), host='localhost', port=43001)
