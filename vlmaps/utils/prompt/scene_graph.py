e2d173ce1758b7972552b5ca5d336317c5c009ef
vuduc153 <vuductran153@gmail.com>
2024-08-26 23:56:29 +1000

    Added scene graph for UX lab

--- vlmaps/utils/prompt/scene_graph.py
+++ vlmaps/utils/prompt/scene_graph.py
@@ -4,28 +4,81 @@ SCENE_GRAPH = """
     "additional_detail": "Melbourne Connect",
     "rooms": [
         {
-            "coordinate": [5.7326, 7.0397, 0.0762, 0.0000, 0.0000, -0.8627, 0.5057],
-            "scene_category": "IxT lab",
-            "additional_detail": "Interactive user interface laboratory",
+            "scene_category": "UX lab",
+            "additional_detail": "user experience laboratory",
             "objects": [
                 {
-                    "class_": "workstation",
-                    "coordinate": [-1.3229, 3.2359, 0.0762, 0.0000, 0.0000, -0.4356, 0.9001],
+                    "class_": "mirror",
+                    "coordinate": [0.2103, 0.2772, 0.0762, 0.0000, 0.0000, -0.4675, 0.8840],
                     "additional_detail": ""
                 }, {
-                    "class_": "tv",
-                    "coordinate": [2.8889, 6.7676, 0.0762, 0.0000, 0.0000, 0.4850, 0.8745],
-                    "additional_detail": "big display"
+                    "class_": "white robot",
+                    "coordinate": [-0.0935, -0.2019, 0.0762, 0.0000, 0.0000, 0.2360, 0.9717],
+                    "additional_detail": ""
                 }, {
-                    "class_": "3d printer",
-                    "coordinate": [1.0564, 1.1280, 0.0762, 0.0000, 0.0000, -0.4669, 0.8843],
-                    "additional_detail": "can be used for students' projects"
+                    "class_": "laptop next to a white robot",
+                    "coordinate": [-0.3172, 0.9551, 0.0762, 0.0000, 0.0000, 0.2709, 0.9626],
+                    "additional_detail": ""
+                }, {
+                    "class_": "white board",
+                    "coordinate": [0.1540, -2.3730, 0.0762, 0.0000, 0.0000, -0.7747, 0.6323],
+                    "additional_detail": ""
                 },
                 {
-                    "class_": "mirror",
-                    "coordinate": [-1.4979, 6.2643, 0.0762, 0.0000, 0.0000, 0.8149, 0.5796],
+                    "class_": "couch",
+                    "coordinate": [4.2509, -8.8848, 0.0762, 0.0000, 0.0000, -0.4497, 0.8932],
                     "additional_detail": ""
-                }
+                },
+                {
+                    "class_": "kitchen counter",
+                    "coordinate": [6.7484, -8.0333, 0.0762, 0.0000, 0.0000, -0.3477, 0.9376],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "desk next to a white robot",
+                    "coordinate": [-0.2819, 0.8064, 0.0762, 0.0000, 0.0000, 0.2750, 0.9615],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "desk",
+                    "coordinate": [-0.8504, 0.9253, 0.0762, 0.0000, 0.0000, 0.7741, 0.6331],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "carton box",
+                    "coordinate": [-1.2037, 0.8732, 0.0762, 0.0000, 0.0000, 0.8454, 0.5341],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "coffee machine",
+                    "coordinate": [6.7484, -8.0333, 0.0762, 0.0000, 0.0000, -0.3477, 0.9376],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "white box",
+                    "coordinate": [6.3634, -8.1088, 0.0762, 0.0000, 0.0000, -0.6936, 0.7204],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "black box",
+                    "coordinate": [6.7373, -8.0245, 0.0762, 0.0000, 0.0000, 0.1209, 0.9927],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "chair",
+                    "coordinate": [-0.3172, 0.9551, 0.0762, 0.0000, 0.0000, 0.2709, 0.9626],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "chair in the kitchen",
+                    "coordinate": [6.3634, -8.1088, 0.0762, 0.0000, 0.0000, -0.6936, 0.7204],
+                    "additional_detail": ""
+                },
+                {
+                    "class_": "television",
+                    "coordinate": [5.4294, -8.9272, 0.0762, 0.0000, 0.0000, 0.8602, 0.5099],
+                    "additional_detail": ""
+                },
             ]
         }
     ]
